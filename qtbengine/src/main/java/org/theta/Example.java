838ad6e73533baa608058f4901f21fc93002edbd
Alex D Kurasoff <alex.kurasoff@gmail.com>
2025-05-20 19:31:23 +0300

    Update Example.java

--- qtbengine/src/main/java/org/theta/Example.java
+++ qtbengine/src/main/java/org/theta/Example.java
@@ -14,131 +14,136 @@ import org.theta.services.BasicEngine;
 import jakarta.xml.bind.JAXBException;
 
 /**
- * Main class to run the basic example 
+ * Main class to run the basic example for processing forecasting medicine data.
  */
 public class Example {
 
-	/**
-	 * Main method to run from command line. Mainly for performance assessment
-	 * purpose
-	 * 
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		if (args.length == 1) {
-			File dir = new File(args[0]);
-			if (dir.isDirectory()) {
-				File[] directoryListing = dir.listFiles();
-				if (directoryListing != null && directoryListing.length>0) {
-					Date prevTime = new Date();
-					Date start = new Date();
-					int count = 0;
-					File log = new File("log.txt");
-					PrintWriter logStream = new PrintWriter(log);
-					for (File child : directoryListing) {
-						if (child.getName().toUpperCase().endsWith(".QTB")) {
-							try {
-								FileInputStream stream = new FileInputStream(child);
-								log("Processing " + child.getName(), logStream);
-								Forecast forecast = Calculator.instanceOf().loadForecastFromStream(stream, child.getName());
-								forecast = BasicEngine.calculateDailyForecast(forecast);
-								log("processed", logStream);
-								prevTime = printDiag(prevTime, logStream);
-								logResults(forecast, 2,logStream);
-							} catch (FileNotFoundException e) {
-								e.printStackTrace();
-							} catch (JAXBException e) {
-								e.printStackTrace();
-							}
-						}
-						count++;
-					}
-					Date endTime = new Date();
-					log("Total elapsed " + ((endTime.getTime() - start.getTime()) / 1000) + " sec", logStream);
-					log("Processed " + count + " forecasts", logStream);
-					log("Average calculation time is " + ((endTime.getTime() - start.getTime()) / 1000) / count
-							+ " sec", logStream);
-					logStream.flush();
-					logStream.close();
-				} else {
-					System.err.println(
-							"Usage: quantbcalc qtb_directory, instead " + dir.getAbsolutePath());
-				}
-			} else {
-				System.err.println("Usage: quantbcalc qtb_directory, instead " + dir.getAbsolutePath());
-			}
-		} else {
-			System.err.println("Usage: quantbcalc qtb_directory");
-		}
+    /**
+     * Main method to execute processing from the command line.
+     * Used primarily for performance assessment.
+     * 
+     * @param args Command-line arguments, expected to contain a directory path.
+     * @throws IOException If an I/O error occurs while handling files.
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length == 1) {
+            // Validate provided argument as a directory
+            File dir = new File(args[0]);
+            if (dir.isDirectory()) {
+                File[] directoryListing = dir.listFiles();
+                if (directoryListing != null && directoryListing.length > 0) {
+                    Date prevTime = new Date();
+                    Date start = new Date();
+                    int count = 0;
+                    
+                    // Initialize a log file to store processing details
+                    File log = new File("log.txt");
+                    PrintWriter logStream = new PrintWriter(log);
+                    
+                    // Iterate over each file in the directory
+                    for (File child : directoryListing) {
+                        if (child.getName().toUpperCase().endsWith(".QTB")) {
+                            try {
+                                // Load forecast data from the input file
+                                FileInputStream stream = new FileInputStream(child);
+                                log("Processing " + child.getName(), logStream);
+                                Forecast forecast = Calculator.instanceOf().loadForecastFromStream(stream, child.getName());
 
-	}
+                                // Perform daily forecast calculations
+                                forecast = BasicEngine.calculateDailyForecast(forecast);
+                                log("processed", logStream);
 
-	/**
-	 * Output random lines from forecasting results to result.xml file
-	 * For example only
-	 * @param forecast
-	 * @param linesPerMedicine
-	 * @param logStream 
-	 */
-	private static void logResults(Forecast forecast, int linesPerMedicine,  PrintWriter logStream) {
-		try {
-			// Convert list to XML byte array
-	        byte[] xmlBytes;
-			xmlBytes = ForecastingMedicineConverter.convertToXmlBytes(forecast.getMedicines());
-			// Store in result.xml
-	        ForecastingMedicineConverter.storeXmlFile(xmlBytes, "result.xml");
-	        System.out.println("XML file saved successfully.");
-		} catch (JAXBException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+                                // Log memory and execution diagnostics
+                                prevTime = printDiag(prevTime, logStream);
 
-        
-	}
+                                // Save processed forecast results
+                                logResults(forecast, 2, logStream);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (JAXBException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        count++;
+                    }
+                    
+                    // Calculate total processing time
+                    Date endTime = new Date();
+                    log("Total elapsed " + ((endTime.getTime() - start.getTime()) / 1000) + " sec", logStream);
+                    log("Processed " + count + " forecasts", logStream);
+                    log("Average calculation time is " + ((endTime.getTime() - start.getTime()) / 1000) / count + " sec", logStream);
+                    
+                    // Finalize logging output
+                    logStream.flush();
+                    logStream.close();
+                } else {
+                    System.err.println("Usage: quantbcalc qtb_directory, instead " + dir.getAbsolutePath());
+                }
+            } else {
+                System.err.println("Usage: quantbcalc qtb_directory, instead " + dir.getAbsolutePath());
+            }
+        } else {
+            System.err.println("Usage: quantbcalc qtb_directory");
+        }
+    }
 
+    /**
+     * Logs processing results to an XML file for demonstration purposes.
+     * 
+     * @param forecast Forecast object containing the calculated data.
+     * @param linesPerMedicine Number of lines per medicine entry (for example use).
+     * @param logStream Stream to log output messages.
+     */
+    private static void logResults(Forecast forecast, int linesPerMedicine, PrintWriter logStream) {
+        try {
+            // Convert medicine list to XML byte array
+            byte[] xmlBytes = ForecastingMedicineConverter.convertToXmlBytes(forecast.getMedicines());
 
-	/**
-	 * Log it to console and file
-	 * 
-	 * @param message
-	 * @param logStream
-	 */
-	private static void log(String message, PrintWriter logStream) {
-		System.out.println(message);
-		logStream.println(message);
-	}
+            // Store XML data in result.xml
+            ForecastingMedicineConverter.storeXmlFile(xmlBytes, "result.xml");
 
-	/**
-	 * Print diagnostic data
-	 * 
-	 * @param prevTime  when printDiag has been called before.
-	 * @param logStream
-	 */
-	private static Date printDiag(Date prevTime, PrintWriter logStream) {
-		int mb = 1024 * 1024;
-		log("<performance>",logStream);
-		// Getting the runtime reference from system
-		Runtime runtime = Runtime.getRuntime();
+            System.out.println("XML file saved successfully.");
+        } catch (JAXBException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		log("  ##### Heap utilization statistics [MB] #####", logStream);
+    /**
+     * Logs the given message to both console and file.
+     * 
+     * @param message The message to be logged.
+     * @param logStream The output stream to write log data.
+     */
+    private static void log(String message, PrintWriter logStream) {
+        System.out.println(message);
+        logStream.println(message);
+    }
 
-		// Print used memory
-		log("  Used Memory:" + (runtime.totalMemory() - runtime.freeMemory()) / mb, logStream);
+    /**
+     * Prints diagnostic information such as memory usage and execution time.
+     * 
+     * @param prevTime Previous recorded timestamp for elapsed time calculation.
+     * @param logStream Log stream to store performance metrics.
+     * @return The updated timestamp for further diagnostics.
+     */
+    private static Date printDiag(Date prevTime, PrintWriter logStream) {
+        int mb = 1024 * 1024;
+        log("<performance>", logStream);
 
-		// Print free memory
-		log("  Free Memory:" + runtime.freeMemory() / mb, logStream);
+        // Retrieve runtime memory details
+        Runtime runtime = Runtime.getRuntime();
 
-		// Print total available memory
-		log("  Total Memory:" + runtime.totalMemory() / mb, logStream);
+        log("  ##### Heap utilization statistics [MB] #####", logStream);
+        log("  Used Memory: " + (runtime.totalMemory() - runtime.freeMemory()) / mb, logStream);
+        log("  Free Memory: " + runtime.freeMemory() / mb, logStream);
+        log("  Total Memory: " + runtime.totalMemory() / mb, logStream);
+        log("  Max Memory: " + runtime.maxMemory() / mb, logStream);
 
-		// Print Maximum available memory
-		log("  Max Memory:" + runtime.maxMemory() / mb, logStream);
+        // Log elapsed execution time
+        Date ret = new Date();
+        log("  Elapsed " + ((ret.getTime() - prevTime.getTime()) / 1000) + " sec", logStream);
+        log("</performance>", logStream);
 
-		// Print time elapsed
-		Date ret = new Date();
-		log("  Elapsed " + ((ret.getTime() - prevTime.getTime()) / 1000) + " sec", logStream);
-		log("</performance>",logStream);
-		return ret;
-	}
+        return ret;
+    }
 }
